16d5472608f2fe59bc8ac6f62674c01a23c31ae1
Kazi Arham <kaziarham2003@outlook.com>
2024-09-16 19:51:55 +0100

    Organised the implementation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("com.karumi:dexter:6.2.3")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.karumi:dexter:6.2.3")
 }
